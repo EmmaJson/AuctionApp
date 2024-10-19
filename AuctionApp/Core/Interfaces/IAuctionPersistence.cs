1a61a9e4ee01972a285539422ae0347602937b1e
Emma Johansson <emmamhm2@gmail.com>
2024-10-19 20:23:26 +0200

    MySqlAuctionPersistence.cs added IAuctionPersistence.cs added

--- AuctionApp/Core/Interfaces/IAuctionPersistence.cs
+++ AuctionApp/Core/Interfaces/IAuctionPersistence.cs
@@ -2,5 +2,9 @@
 
 public interface IAuctionPersistence
 {
-    
+    List<Auction> GetAuctions();
+    List<Auction> GetUserWonAuctions(string userName);
+    List<Auction> GetUserActiveAuctions(string userName);
+    Auction GetById(int id); 
+    void Save(Auction auction);
 }
--- AuctionApp/Persistence/MySqlAuctionPersistence.cs
+++ AuctionApp/Persistence/MySqlAuctionPersistence.cs
@@ -1,6 +1,97 @@
-﻿namespace AuctionApp.Persistence;
+﻿using System.Data;
+using AuctionApp.Core;
+using AuctionApp.Core.Interfaces;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 
-public class MySqlAuctionPersistence
+namespace AuctionApp.Persistence;
+
+public class MySqlAuctionPersistence : IAuctionPersistence
 {
+    private readonly AuctionDbContext _dbContext;
+    private readonly IMapper _mapper;
     
+    public MySqlAuctionPersistence(AuctionDbContext dbContext, IMapper mapper)
+    {
+        _dbContext = dbContext;
+        _mapper = mapper;
+    }
+    
+    public List<Auction> GetAuctions()
+    {
+        var auctionDbs = _dbContext.AuctionDbs
+            .Where(a => a.EndDate > DateTime.Now)
+            .ToList();
+        
+        List<Auction> result = new List<Auction>();
+        foreach (AuctionDb adb in auctionDbs)
+        {
+            Auction auction = _mapper.Map<Auction>(adb);
+            result.Add(auction);
+        }
+        result.Sort();
+        return result;
+    }
+
+    public List<Auction> GetUserWonAuctions(string userName)
+    {
+        var auctionDbs = _dbContext.AuctionDbs
+            .Where(a => a.EndDate <= DateTime.Now)
+            .ToList();
+        
+        List<Auction> result = new List<Auction>();
+        foreach (AuctionDb adb in auctionDbs)
+        {
+            Auction auction = _mapper.Map<Auction>(adb);
+            if (auction.Bids.First().UserName.Equals(userName))
+            {
+                result.Add(auction);
+            }
+        }
+        result.Sort();
+        return result;
+    }
+
+    public List<Auction> GetUserActiveAuctions(string userName)
+    {
+        var auctionDbs = _dbContext.AuctionDbs
+            .Where(a => a.EndDate > DateTime.Now)
+            .ToList();
+        
+        List<Auction> result = new List<Auction>();
+        foreach (AuctionDb adb in auctionDbs)
+        {
+            Auction auction = _mapper.Map<Auction>(adb);
+            if (auction.Bids.Any(b => b.UserName == userName)) {
+                result.Add(auction);
+            }
+        }
+        result.Sort();
+        return result;
+    }
+    
+    public Auction GetById(int id)
+    {
+        AuctionDb auctionDb = _dbContext.AuctionDbs
+            .Where(a => a.Id == id)
+            .Include(a => a.BidDbs)
+            .FirstOrDefault();   // Null if not found!
+        
+        if (auctionDb == null) throw new DataException("Project not found");
+        
+        Auction auction = _mapper.Map<Auction>(auctionDb);
+        foreach (BidDb bidDb in auctionDb.BidDbs)
+        {
+            Bid bid = _mapper.Map<Bid>(bidDb);
+            auction.AddBid(bid);
+        }
+        return auction;
+    }
+
+    public void Save(Auction auction)
+    {
+        AuctionDb adb = _mapper.Map<AuctionDb>(auction);
+        _dbContext.AuctionDbs.Add(adb);
+        _dbContext.SaveChanges();
+    }
 }
